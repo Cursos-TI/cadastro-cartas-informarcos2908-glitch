fee93e89d5fb7ddfbb96c96387ee9e7db3133133
informarcos2908-glitch <informarcos2908@gmail.com>
2026-03-03 00:25:42 +0000

    Neesse ponto eu criei o cadstro de carta2 mas contém erros

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,13 +7,13 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
-    char estado1;            //'A' a 'H'
-    char codigo1[4];         // Ex: "A01"
-    char nomeCidade1[50];
-    int populacao1;
-    float area1;
-    float pib1;
-    int pontosTuristicos1;
+    char estado1, estado2;            //'A' a 'H'
+    char codigo1, codigo2[4];         // Ex: "A01"
+    char nomeCidade1, nomeCidade2[50];
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontosTuristicos1, pontosTuristicos2;
 
       printf("-- Cadastro de Cartas: Super Trunfo Países --\n\n");
 
@@ -22,7 +22,7 @@ int main() {
       scanf("%c", &estado1);
 
       printf("Digite o código da carta (Ex: A01): ");
-      scanf("%s", codigo1);
+      scanf(" %s", codigo1);
 
       //Agora estou usando %s simples, que aceita nomes como: 'saoPaulo'
 
@@ -41,10 +41,34 @@ int main() {
       printf("Pontos Turísticos: ");
       scanf("%d", &pontosTuristicos1);
 
+      printf("Digite a letra do estado (A-H): ");
+      scanf("%c", &estado2);
+
+      printf("Digite o código da carta (Ex: A01): ");
+      scanf(" %s", codigo2);
+
+      //Agora estou usando %s simples, que aceita nomes como: 'saoPaulo'
+
+      printf("Digite o nome da cidade: (Ex: saoPaulo) ");
+      scanf(" %s", nomeCidade2);
+
+      printf("População: ");
+      scanf("%d", &populacao2);
+
+      printf("Área (Km2²): ");
+      scanf("%f", &area2);
+
+      printf("PIB: ");
+      scanf("%f", &pib2);
+
+      printf("Pontos Turísticos: ");
+      scanf("%d", &pontosTuristicos2);
+
 
   // Área para exibição dos dados da cidade
 
   // -- Exibição dos dados --
+      //Cadastrando Carta 1
       printf("\n*********************************\n");
       printf("Carta cadastrada com sucesso:\n");
       printf("ID: %s - Estado: %c\n", codigo1, estado1);
@@ -54,8 +78,20 @@ int main() {
       printf("PIB: %.2f \n", pib1);
       printf("Turismo: %d pontos\n", pontosTuristicos1);
       printf("*********************************\n");
+
+      // -- Exibição dos dados --
+      //Cadastrando Carta 2
+      printf("\n*********************************\n");
+      printf("Carta cadastrada com sucesso:\n");
+      printf("ID: %s - Estado: %c\n", codigo2, estado2);
+      printf("Cidade: %s\n", nomeCidade2);
+      printf("População: %d habitantes\n", populacao2);
+      printf("Área: %.2f (km2²)\n", area2);
+      printf("PIB: %.2f \n", pib2);
+      printf("Turismo: %d pontos\n", pontosTuristicos2);
+      printf("*********************************\n");
       
-      printf("Até o momento o meu código nível iniciante está muito bom!\n");
+      //printf("Até o momento o meu código nível iniciante está muito bom!\n");
 
 return 0;
 } 
